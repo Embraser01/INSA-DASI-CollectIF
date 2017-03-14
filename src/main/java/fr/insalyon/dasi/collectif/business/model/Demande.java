df001472c3449ca24caf297528544140307d2e26
Marc-Antoine FERNANDES <embraser01@gmail.com>
2017-03-14 15:23:21 +0100

    Add link to an event

--- src/main/java/fr/insalyon/dasi/collectif/business/model/Demande.java
+++ src/main/java/fr/insalyon/dasi/collectif/business/model/Demande.java
@@ -25,6 +25,9 @@ public class Demande implements Serializable {
     @ManyToOne
     private Adherent adherent;
 
+    @ManyToOne
+    private Evenement evenement;
+
     protected Demande() {
     }
 
